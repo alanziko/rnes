2896d946cfa6c1ee174c05b5378e68aba686cad5
alanziko <wxxalan@gmail.com>
2026-05-03 17:38:34 +0200

    feat: add ADC instruction

--- src/opcode/instructions.rs
+++ src/opcode/instructions.rs
@@ -1,5 +1,3 @@
-use std::collections::btree_map::Values;
-
 use crate::{
     bus::Bus,
     cpu::{CPU, StatusRegister},
@@ -22,9 +20,63 @@ impl Operand {
 }
 
 pub fn add_with_carry(cpu: &mut CPU, bus: &mut dyn Bus, operand: Operand) {
-    let operand = operand.fetch(bus).unwrap();
-    let (sum, carry) = cpu.ac.overflowing_add(operand);
-    cpu.ac = sum;
-    cpu.sr.set_flag(StatusRegister::Carry, carry);
-    // Not done
+    let a = cpu.ac;
+    let m = operand.fetch(bus).unwrap();
+    let c = cpu.sr.contains(StatusRegister::Carry);
+    let sum = a as u16 + m as u16 + c as u16;
+
+    let result = sum as u8;
+
+    cpu.sr.set_flag(StatusRegister::Carry, sum > 0xFF);
+    cpu.sr.set_flag(StatusRegister::Zero, result == 0);
+    let negative = (result & 0x80) != 0;
+    cpu.sr.set_flag(StatusRegister::Negative, negative);
+    let overflow = (a ^ result) & (m ^ result) & 0x80 != 0;
+    cpu.sr.set_flag(StatusRegister::Overflow, overflow);
+
+    cpu.ac = result;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::bus::Memory;
+
+    use super::*;
+
+    #[test]
+    fn test_add_with_carry() {
+        let mut cpu = CPU::default();
+        let mut bus = Memory::new();
+
+        bus.set_byte(0x0000, 30);
+
+        cpu.ac = 0;
+
+        // Carry
+        add_with_carry(&mut cpu, &mut bus, Operand::Address(0x0000));
+        assert_eq!(cpu.ac, 30);
+
+        add_with_carry(&mut cpu, &mut bus, Operand::Value(255));
+        assert_eq!(cpu.ac, 29);
+        assert!(cpu.sr.contains(StatusRegister::Carry));
+
+        add_with_carry(&mut cpu, &mut bus, Operand::Value(0));
+        assert_eq!(cpu.ac, 30);
+
+        // Oveflow
+        cpu.ac = 100;
+
+        add_with_carry(&mut cpu, &mut bus, Operand::Value(50));
+        assert!(cpu.sr.contains(StatusRegister::Overflow));
+
+        // Negative
+        assert!(cpu.sr.contains(StatusRegister::Negative));
+
+        // Zero
+        cpu.ac = 0;
+        cpu.sr.set_flag(StatusRegister::Carry, false);
+
+        add_with_carry(&mut cpu, &mut bus, Operand::Value(0));
+        assert!(cpu.sr.contains(StatusRegister::Zero));
+    }
 }
